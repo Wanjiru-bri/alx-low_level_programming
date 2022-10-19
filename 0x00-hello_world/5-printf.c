6d8fdebf80b61403bf7d1acc9fbc4273f5839e29
Bridgit Wanjiru Nyokabi <bridgitwanjiru50@gmail.com>
2022-10-18 17:25:46 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main-entry point for the program
+ *
+ * Return:0 if no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
